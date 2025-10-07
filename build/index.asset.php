f66fe419634368e067ddf0fcf679647043b2923e
Thomas Finley <tfinley160@gmail.com>
2025-10-07 15:07:20 -0400

    fixed issues related to large fonts

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dd00b62de0c3eff8d670');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd6e43c500c262e02353a');
